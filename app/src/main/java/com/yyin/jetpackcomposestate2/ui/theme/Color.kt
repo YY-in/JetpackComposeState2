48d80a3a00caf29bb6b80e4dcb1927f2e6ff2d76
YY-in <139803551@qq.com>
2022-09-19 16:26:33 +0800

    add: theme example

--- app/src/main/java/com/yyin/jetpackcomposestate2/ui/theme/Color.kt
+++ app/src/main/java/com/yyin/jetpackcomposestate2/ui/theme/Color.kt
@@ -1,8 +1,27 @@
 package com.yyin.jetpackcomposestate2.ui.theme
 
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+private val Yellow200 = Color(0xffffeb46)
+private val Yellow500 = Color(0xFFFFEB3B)
+private val Yellow400 = Color(0xFFFFEE58)
+private val Blue200 = Color(0xff91a4fc)
+private val Blue700 = Color(0xFF1976D2)
+
+private val DarkColors = darkColors(
+    primary = Yellow200,
+    secondary = Blue200,
+    // ...
+)
+private val LightColors = lightColors(
+    primary = Yellow500,
+    primaryVariant = Yellow400,
+    secondary = Blue700,
+)
